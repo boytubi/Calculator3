c5bbeaeae5f88d928f387c17e0afb88bdf9b918d
boytubi <hoangtruc14@gmail.com>
2018-07-12 10:44:12 +0700

    update code

--- app/src/main/java/com/example/hoangtruc/calculator/MainActivity.java
+++ app/src/main/java/com/example/hoangtruc/calculator/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.hoangtruc.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+CalculatorFrag calculatorFrag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        calculatorFrag =new CalculatorFrag();
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frag,calculatorFrag);
+        fragmentTransaction.commit();
     }
+
 }
+
